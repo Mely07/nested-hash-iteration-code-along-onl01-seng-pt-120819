0c1b4b2eb6b52a1c9d32a0b824ffa4136e780248
Damely Tineo <damely.tineo07@gmail.com>
2019-12-22 00:43:55 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
   # This is the array we will be passing into the remove_strawberry method
   # contacts = {
@@ -16,5 +16,5 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    binding.pry
+
 end
